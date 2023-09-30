714ad104e6ec1c7a865b50849f7c2e4f34d50b3e
MC4MP02 <mcampocabrera2013@gmail.com>
2023-09-30 13:14:15 +0200

    a

--- src/baseNoStates/DoorState.java
+++ src/baseNoStates/DoorState.java
@@ -7,12 +7,40 @@ abstract class DoorState{
   public DoorState(Door door) {
     this.door=door;
   }
-  public abstract void open();
-  public abstract void close();
-  public void lock() {
 
+  public void open() {
+    if (canOpen()) {
+      System.out.println("Opening the door.");
+      door.setClosed(false);
+    } else {
+      System.out.println("Can't open the door. It's locked.");
+    }
   }
-  public void unlock() {
 
+  public void close() {
+    if(!door.isClosed()) {
+      System.out.println("Closing the door.");
+      door.setClosed(true);
+    }
+    else
+    {
+      System.out.println("Can't close the door. Is already closed.");
+    }
+  }
+
+  public void lock() {
+    System.out.println("Locking the door.");
+    door.setState(new Locked(door));
+  }
+
+  public void unlock() {
+    System.out.println("Unlocking the door.");
+    door.setState(new Unlocked(door));
+  }
+  public String getName() {
+    return name;
+  }
+  public boolean canOpen() {
+    return true;
   }
 }
