0b0383734467938ab979da62bcb43375a94775b4
MC4MP02 <mcampocabrera2013@gmail.com>
2023-09-30 17:02:59 +0200

    lock-unlock done

--- src/baseNoStates/Locked.java
+++ src/baseNoStates/Locked.java
@@ -8,15 +8,15 @@ public class Locked extends DoorState {
   }
 
   public void open() {
-
+    System.out.println("Can't open the door " + door.getId() + " because it's locked");
   }
 
   public void close() {
-
+    System.out.println("Can't close the door " + door.getId() + " because it's locked");
   }
 
   public void lock() {
-
+    System.out.println("Can't lock the door " + door.getId() + " because it's already locked");
   }
   public void unlock() {
     if(name.equals(States.LOCKED)) {
